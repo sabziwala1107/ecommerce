5f08cbf0ea400e79bcbc678febff3d9fb6ae1b69
sabziwala1107 <sabziwala1107@gmail.com>
2022-08-19 20:21:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ecommerceapp/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.ecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signup(View view) {
+
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+    }
+
+    public void signin(View view){
+
+
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+
+    }
+}
